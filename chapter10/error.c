b310447da9d1bf159c665cd17a250a7db3a14547
Chaniel <netqyq@gmail.com>
2014-11-26 21:31:11 +0800

    add error.c file

--- chapter10/error.c
+++ chapter10/error.c
@@ -1,1 +1,22 @@
-//
+// error.c 
+// error handlling
+
+
+#include <errno.h>
+
+void error(char *msg)
+{
+  fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+  exit(1);
+}
+
+// usage : include error function and call it;
+pid_t pid = fork();
+if (pid == -1){
+  error("can't clone process\n");
+}
+
+if (execl(...) == -1){
+  error("can't run the script");
+}
+
