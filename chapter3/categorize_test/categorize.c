4bdf5f15af69cfd585e783b8bb63546f8a23a1ae
chaniel <netqyq@gmail.com>
2016-05-18 21:22:49 +0800

    change some files.

--- chapter3/categorize_test/categorize.c
+++ chapter3/categorize_test/categorize.c
@@ -2,21 +2,21 @@
  #include <stdlib.h>
  #include <string.h>
  
- int main(int argc, char* args[])
+ int main(int argc, char* argv[])
  {
    char line[80];
    if (argc != 6) {
      fprintf(stderr, "You need to give 5 arguments\n");
      return 1;
    }
-   FILE* in = fopen("gpsdata.data", "r");
-   FILE* file1 = fopen(args[2], "w");
-   FILE* file2 = fopen(args[4], "w");
-   FILE* file3 = fopen(args[5], "w");
+   FILE* in = fopen("spllky.csv", "r");
+   FILE* file1 = fopen(argv[2], "w");
+   FILE* file2 = fopen(argv[4], "w");
+   FILE* file3 = fopen(argv[5], "w");
    while (fscanf(in, "%79[^\n]\n", line) == 1) {
-     if (strstr(line, args[1]))
+     if (strstr(line, argv[1]))
        fprintf(file1, "%s\n", line);
-     else if (strstr(line, args[3]))
+     else if (strstr(line, argv[3]))
        fprintf(file2, "%s\n", line);
      else
        fprintf(file3, "%s\n", line);
