da0a10b1e226db41814fd67de99f5b83d9c240d1
chaniel <netqyq@gmail.com>
2016-05-24 22:22:09 +0800

    add ch6.

---
+++ chapter6/travel_in_islands.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct island{
+    char *name;
+    char *opens;
+    char *closes;
+    struct island *next;
+} island;
+
+island* create(char *name){
+    island *i = malloc(sizeof(island));
+    i->opens = "09:00";
+    i->closes = "17:00";
+    i->next = NULL;
+    return i;
+}
+
+ 
+
+int main(){
+    char *i_name = "totyo";
+    island *i = create(i_name);
+    printf("%s\n", i->opens);
+    return 0;
+}
